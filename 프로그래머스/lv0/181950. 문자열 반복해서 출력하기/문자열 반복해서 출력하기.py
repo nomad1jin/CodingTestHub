6df7023d4c03e214873d66944a35c9f9274c31be
nomad1jin <167287284+nomad1jin@users.noreply.github.com>
2026-05-14 20:00:58 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
+++
@@ -1,7 +0,0 @@
-str, n = input().strip().split(' ')
-n = int(n)
-arr = []
-for i in range(n):
-    arr.append(str)
-
-print(''.join(arr))
--- 프로그래머스/lv0/250132. ［PCCE 기출문제］ 2번 ／ 피타고라스의 정리/［PCCE 기출문제］ 2번 ／ 피타고라스의 정리.py
+++
@@ -1,5 +0,0 @@
-a = int(input())
-c = int(input())
-
-b_square = c * c - a * a
-print(b_square)
